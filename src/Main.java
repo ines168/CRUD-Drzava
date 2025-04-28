3122c8837ff7d85a193c8ee632d8de414f784fb3
Ines <ines.cvek16@gmail.com>
2025-04-28 19:54:35 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DataSource dataSource = createDataSource();
+
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println("Uspješno ste spojeni na bazu podataka!");
+
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                ispisMenija();
+                System.out.print("Vaš izbor: ");
+                int izbor = sc.nextInt();
+                sc.nextLine(); //očisti nextInt enter
+
+                switch (izbor) {
+                    case 1 -> createDrzava(connection, sc);
+                    case 2 -> updateDrzava(connection, sc);
+                    case 3 -> deleteDrzava(connection, sc);
+                    case 4 -> indexDrzava(connection);
+                    case 5 -> {
+                        System.out.println("Kraj programa.");
+                        return;
+                    }
+                    default -> System.err.println("Nije validan unos. Pokušaj ponovno!");
+                }
+
+            }
+
+        }
+        catch (SQLException e) {
+            System.err.println("Greška prilikom spajanja na bazu podataka.");
+            e.printStackTrace();
+        }
+
+    }
+    public static void ispisMenija() {
+        System.out.println("\nIzaberi slijedeće:");
+        System.out.println("1 - Unos nove države");
+        System.out.println("2 - izmjena postojeće države");
+        System.out.println("3 - brisanje postojeće države");
+        System.out.println("4 - prikaz svih država sortiranih po nazivu");
+        System.out.println("5 - end");
+    }
+
+    public static void createDrzava(Connection connection, Scanner sc) throws SQLException {
+        Statement stmt = connection.createStatement();
+        System.out.println("Upiši naziv nove države:");
+        String nazivDrzave = sc.nextLine();
+        String sql = String.format("INSERT INTO Drzava(Naziv) VALUES ('%s')", nazivDrzave);
+        int rowAffected = stmt.executeUpdate(sql);
+        System.out.println(rowAffected > 0 ? "Država je uspješno unesena!" : "Nije unešena država!");
+        stmt.close();
+    }
+    public static void updateDrzava(Connection connection, Scanner sc) throws SQLException {
+        Statement stmt = connection.createStatement();
+        System.out.println("Upiši ID države koju želiš izmijeniti:");
+        int idDrzave = sc.nextInt();
+        System.out.println("Upiši novi naziv države:");
+        String nazivDrzave = sc.next();
+        String sql = String.format("UPDATE Drzava SET Naziv = '%s' WHERE IDDrzava = %s", nazivDrzave, idDrzave);
+        int rowAffected = stmt.executeUpdate(sql);
+        System.out.println(rowAffected > 0 ? "Država je uspješno preimenovana!" : "Nije pronađena država!");
+        stmt.close();
+    }
+
+    public static void deleteDrzava(Connection connection, Scanner sc) throws SQLException {
+        Statement stmt = connection.createStatement();
+        System.out.println("Upiši ID države koju želiš obrisati:");
+        int idDrzave = sc.nextInt();
+        String sql = String.format("DELETE FROM Drzava WHERE IDDrzava=%s", idDrzave);
+        int rowAffected = stmt.executeUpdate(sql);
+        System.out.println(rowAffected > 0 ? "Država je uspješno obrisana!" : "Nije pronađena država!");
+        stmt.close();
+    }
+
+    public static void indexDrzava(Connection connection) throws SQLException {
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT IDDrzava, Naziv FROM Drzava ORDER BY Naziv");
+        System.out.println("\nPopis država po nazivu:");
+
+        while (rs.next()) {
+            int id = rs.getInt("IDDrzava");
+            String naziv = rs.getString("Naziv");
+            System.out.println(naziv + ", ID: " + id );
+        }
+        rs.close();
+        stmt.close();
+    }
+
+    private static DataSource createDataSource() {
+        SQLServerDataSource ds = new SQLServerDataSource();
+        ds.setServerName("localhost");
+        ds.setPortNumber(1433);
+        ds.setDatabaseName("AdventureWorksOBP");
+        ds.setUser("sa");
+        ds.setPassword("SQL");
+        ds.setEncrypt(false);
+        return ds;
+    }
+}
